eeafdfbe7fa82b99256a088dc385fd9e7e7fb00a
Rinaldy Dwi Istanto <rinaldydwii@gmail.com>
2019-04-06 22:18:39 +0800

    update home screen view

--- src/containers/HomeScreen.js
+++ src/containers/HomeScreen.js
@@ -1,6 +1,11 @@
 import React, { Component } from "react";
 import { View, Text, StyleSheet } from "react-native";
+import { Logo, Menu } from "../components";
+import { UserSVG } from "../assets";
+import colors from "../styles/colors";
+import fonts from "../styles/fonts";
 
+// TODO: GET NAME FROM API
 class HomeScreen extends Component {
     static navigationOptions = {
         header: null
@@ -8,7 +13,12 @@ class HomeScreen extends Component {
     render() {
         return (
             <View style={styles.container}>
-                <Text>HomeScreen</Text>
+                <Logo name={"Nama Saya"} />
+                <View style={styles.userContainer}>
+                    <UserSVG width={30} height={30} />
+                    <Text style={styles.userNameText}>Welcome, Nama Saya</Text>
+                </View>
+                <Menu />
             </View>
         );
     }
@@ -19,6 +29,22 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         alignItems: 'center',
-        justifyContent: 'center'
-    }
+        paddingHorizontal: 20
+    },
+    userContainer: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        paddingVertical: 5,
+        paddingHorizontal: 10,
+        marginTop: 30,
+        borderColor: "#bdbdbd",
+        borderWidth: 1,
+        borderRadius: 8,
+        width: "100%"
+    },
+    userNameText: {
+        marginLeft: 10,
+        color: colors.black,
+        ...fonts.openSans
+    },
 });
