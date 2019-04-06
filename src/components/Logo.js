fb104073e3dcdfd87d51aaa69986dca3a9a29121
Rinaldy Dwi Istanto <rinaldydwii@gmail.com>
2019-04-06 21:33:10 +0800

    update logo component props

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -3,9 +3,9 @@ import { View, Text } from "react-native";
 import { LogoSVG } from "../assets";
 import styles from "./styles";
 
-const Logo = () => (
+const Logo = ({size}) => (
     <View style={styles.logoContainer}>
-        <LogoSVG width={150} height={200} />
+        <LogoSVG width={size ? size : 150} height={size ? size * 1.25 : 200} />
         <Text style={styles.logoText}>Assesment App Rinaldy Dwi Istanto</Text>
     </View>
 )
