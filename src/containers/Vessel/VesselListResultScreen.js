7bfaa006942e59c3dee29f71e3489355cf735889
Rinaldy Dwi Istanto <rinaldydwii@gmail.com>
2019-04-06 23:58:32 +0800

    update vessel list result screen view

--- src/containers/Vessel/VesselListResultScreen.js
+++ src/containers/Vessel/VesselListResultScreen.js
@@ -1,11 +1,34 @@
 import React, { Component } from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, StyleSheet, TouchableOpacity } from "react-native";
+import { VesselList } from "../../components";
+import { AddSVG } from "../../assets";
 
 class VesselListResultScreen extends Component {
+    static navigationOptions = ({navigation}) => ({
+        title: navigation.state.params.terminal,
+        headerRight: (
+            <TouchableOpacity
+                onPress={() => navigation.navigate('CreateVessel')}
+                style={styles.addButtonContainer}
+            >
+                <AddSVG width={24} height={24}  />
+            </TouchableOpacity>
+        )
+    })
+    constructor() {
+        super();
+        this.state = {
+            terminal: null
+        }
+    }
+    componentDidMount() {
+        const terminal = this.props.navigation.state.params.terminal
+        this.setState({terminal})
+    }
     render() {
         return (
             <View style={styles.container}>
-                <Text>VesselListResultScreen</Text>
+                <VesselList />
             </View>
         );
     }
@@ -15,7 +38,9 @@ export default VesselListResultScreen;
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center'
+        paddingHorizontal: 20
+    },
+    addButtonContainer: {
+        marginRight: 20,
     }
 });
