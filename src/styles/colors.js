2374588e2338aa8899d3f3e7b957a24d5ae585ad
Rinaldy Dwi Istanto <rinaldydwii@gmail.com>
2019-04-07 07:35:21 +0800

    add blackoverlay color

--- src/styles/colors.js
+++ src/styles/colors.js
@@ -2,5 +2,6 @@ export default colors = {
     black: "#4f4f4f",
     greyTwo: "#828282",
     greyOne: "#bdbdbd",
-    white: "#ffffff"
+    white: "#ffffff",
+    blackOverlay: "rgba(0, 0, 0, 0.4)"
 }
