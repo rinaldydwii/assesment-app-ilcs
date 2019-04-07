6243c169fe20358695be57110603c8841fac666c
Rinaldy Dwi Istanto <rinaldydwii@gmail.com>
2019-04-07 12:02:46 +0800

    update mVessel Redux

--- src/actions/mVesselAction.js
+++ src/actions/mVesselAction.js
@@ -1,6 +1,9 @@
 export const REQUEST_GET_M_VESSEL = "REQUEST_GET_M_VESSEL";
 export const SUCCESS_GET_M_VESSEL = "SUCCESS_GET_M_VESSEL";
 export const FAIL_GET_M_VESSEL = "FAIL_GET_M_VESSEL";
+export const REQUEST_CREATE_M_VESSEL = "REQUEST_CREATE_M_VESSEL";
+export const SUCCESS_CREATE_M_VESSEL = "SUCCESS_CREATE_M_VESSEL";
+export const FAIL_CREATE_M_VESSEL = "FAIL_CREATE_M_VESSEL";
 import API from '../services/constant';
 import request from '../services';
 import handleError from '../helpers/handleError';
@@ -23,6 +26,24 @@ function failGetMVessel(error) {
         error
     }
 }
+function requestCreateMVessel(terminal) {
+    return {
+        type: REQUEST_CREATE_M_VESSEL,
+        terminal
+    }
+}
+function successCreateMVessel(mVessel) {
+    return {
+        type: SUCCESS_CREATE_M_VESSEL,
+        mVessel
+    }
+}
+function failCreateMVessel(error) {
+    return {
+        type: FAIL_CREATE_M_VESSEL,
+        error
+    }
+}
 
 export function getMVesselByTerminal(terminal) {
     return dispatch => {
@@ -30,5 +51,14 @@ export function getMVesselByTerminal(terminal) {
         request.get(API.M_VESSEL+"/"+API.TERMINAL+"/"+terminal)
             .then(response => dispatch(successGetMVessel(response.data.data)))
             .catch(error => dispatch(failGetMVessel(handleError(error))))
+    }
+}
+
+export function createMVessel(m_vessel) {
+    return dispatch => {
+        dispatch(requestCreateMVessel());
+        request.post(API.M_VESSEL, m_vessel)
+            .then(response => dispatch(successCreateMVessel(response.data.data)))
+            .catch(error => dispatch(failCreateMVessel(handleError(error))))
     }
 }
--- src/reducers/mVesselReducer.js
+++ src/reducers/mVesselReducer.js
@@ -1,13 +1,17 @@
 import {
     REQUEST_GET_M_VESSEL,
     SUCCESS_GET_M_VESSEL,
-    FAIL_GET_M_VESSEL
+    FAIL_GET_M_VESSEL,
+    REQUEST_CREATE_M_VESSEL,
+    SUCCESS_CREATE_M_VESSEL,
+    FAIL_CREATE_M_VESSEL
 } from "../actions/mVesselAction";
 
 const INITIAL_STATE = {
     terminal: "",
     mVessels: [],
     loading: false,
+    loadingCreate: false,
     error: ""
 };
 
@@ -29,6 +33,24 @@ const mVesselReducer = (state = INITIAL_STATE, action) => {
             return {
                 ...state,
                 loading: false,
+                error: action.error
+            }
+        case REQUEST_CREATE_M_VESSEL:
+            return {
+                ...state,
+                loadingCreate: true,
+            }
+        case SUCCESS_CREATE_M_VESSEL:
+            let mVessels = state.mVessels
+            return {
+                ...state,
+                loadingCreate: false,
+                mVessels: mVessels.concat([action.mVessel])
+            }
+        case FAIL_CREATE_M_VESSEL:
+            return {
+                ...state,
+                loadingCreate: false,
                 error: action.error
             }
         default:
